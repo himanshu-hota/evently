c7ce1a681eaa03864e4fad53c926ef6af8589642
Himanshu Hota <Himanshuhota55@gmail.com>
2024-02-04 15:18:31 +0530

    form fix

--- src/lib/database/models/event.model.ts
+++ src/lib/database/models/event.model.ts
@@ -9,7 +9,7 @@ export interface IEvent extends Document {
   imageUrl: string;
   startDateTime: Date;
   endDateTime: Date;
-  price: string | number;
+  price: string | undefined;
   isFree: boolean;
   url?: string;
   category: { _id: string; name: string };
